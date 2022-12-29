ec1496b920d4d53a44add8225cfcf3c657ba8dbe
sshd911 <syou20020911@gmail.com>
2022-12-29 22:31:15 +0900

    fix

--- app.py
+++ app.py
@@ -1,39 +1,34 @@
 from flask import Flask, render_template, Response
-import mediapipe as mp
-import numpy as np
+from cvzone.HandTrackingModule import HandDetector
+from main import Main
 import cv2
 
 app = Flask(__name__)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
 
-def gen_frames(mpHands, hands, mpDraw):
+def init(): 
     cap = cv2.VideoCapture(0)
+    detector = HandDetector(detectionCon=0.8, maxHands=1)
+    game = Main("Donut.png")
     while True:
-        gray_img = np.zeros((1000, 1000, 3), np.uint8)
-        suzccess, img = cap.read()
-        if not suzccess:
-            break
-        else:
-            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            results = hands.process(imgRGB)
-            if results.multi_hand_landmarks:
-                for handlms in results.multi_hand_landmarks:
-                    for id, lm in enumerate(handlms.landmark):
-                        h, w, c = imgRGB.shape
-                        cx, cy = int(lm.x*w), int(lm.y*h)
-                        if id in [10, 11, 12]:
-                            cv2.circle(imgRGB, (cx, cy), 15, (139, 0, 0), cv2.FILLED)
-                    mpDraw.draw_landmarks(gray_img, handlms, mpHands.HAND_CONNECTIONS)
-            _, buffer = cv2.imencode('.jpg', gray_img)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        _, img = cap.read()
+        img = cv2.flip(img, 1)
+        hands, img = detector.findHands(img, flipType=False)
+
+        if hands:
+            lmList = hands[0]['lmList']
+            pointIndex = lmList[8][0:2]
+            img = game.update(img, pointIndex)
+        _, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        # key = cv2.waitKey(1)
+        # if key == ord('r'):
+        #     game.gameOver = False
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(mpHands, hands, mpDraw), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(init(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def index():
